3c681f30a4a44fab8ca60bd309dec7c8af45ae39
CSELEC38052 <CSELEC38052@lab.cict>
2023-03-28 13:13:13 +0800

    initial commit

---
+++ server/util.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 28 11:29:47 2023
+
+@author: CSELEC38052
+"""
+
+import json
+import pickle
+import numpy as np
+
+__locations = None
+__data_columns = None
+__model = None
+
+def get_estimated_price(location, sqft, bhk, bath):
+    
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+    
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index >= 0:
+        x[loc_index] = 1
+        
+    return round(__model.predict([x])[0],2)
+
+def get_location_names():
+    global __locations
+    return __locations
+
+def load_saved_artifacts():
+    print('loading saved artifacts..start')
+    global __data_columns
+    global __locations
+    
+    with open("./artifacts/columns.json", 'r') as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:]
+        
+    global __model
+    with open("./artifacts/banglore_home_prices_model.pickle", 'rb') as f:
+        __model = pickle.load(f)
+        
+    print("loading saved artifacts..done!")
+        
+
+if __name__ == '__main__':
+    load_saved_artifacts()
+    print(get_location_names())
+    print(get_estimated_price('1st Phase JP Nagar', 1450, 4, 4))
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
+    print(get_estimated_price('Ejipura', 1000, 2, 2))
+    
